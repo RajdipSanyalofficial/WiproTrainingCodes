969e77e77a2fddc747bae61ca29999b676441cb7
Rajdip-Sanyal <sanyalraj2@gmail.com>
2024-07-19 13:56:03 +0530

    Basic Java Codes of Training

--- Greatest2.java
+++ Greatest2.java
@@ -1,3 +1,17 @@
+/**
+ * Debugging code refers to the process of identifying and fixing errors or bugs in a written code.
+ *  * It is an essential step in the software development cycle to ensure that the code runs seamlessly and produces the desired output.
+ *  * Debugging involves examining the code line by line, identifying the problematic sections, and making the necessary corrections.
+ *  * This can be done using various tools and techniques, such as print statements, debuggers, and logging tools.
+ *  * Effective debugging requires attention to detail, analytical skills, and a solid understanding of programming concepts and languages.
+ *  *____________________________________________________________________________________________________
+ *  * Steps to Debug a Code:
+ *  * 1) Apply breakpoint  by clicking on line number in gutter area
+ *  * 2) Click Debug icon & give inputs - program execution stops at Breakpoint
+ *  * 3) In Debug window click StepOver Icon & examine code
+ */
+
+
 package basics;
 
 import java.io.BufferedReader;
--- ScannerDemo.java
+++ ScannerDemo.java
@@ -3,62 +3,40 @@ package basics;
 import java.util.Scanner;
 
 public class ScannerDemo {
+    public static void main(String[] args){
 
-    public static void main(String[] args)
-    {
         char init;
-        String fullname, firstname;
+        String fullName,firstName;
         int id;
         float marks;
         double salary;
-        Scanner sc =new Scanner(System.in);
 
-        System.out.println("************ Enter Employee Details **************");
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter Employee id:  ");
-        id=sc.nextInt();
+        System.out.println("******************** Enter Employee Details ***********************");
+        System.out.println("Enter employee ID     :");
+        id = scanner.nextInt();
+        System.out.println("Enter Initials        :");
+        init = scanner.next().charAt(0);
+        System.out.println("Enter your first name :");
+        firstName = scanner.next();
+        scanner.nextLine();
+        System.out.println("Enter your full name  :");
+        fullName = scanner.nextLine();
+        System.out.println("Enter your marks      :");
+        marks = scanner.nextFloat();
+        System.out.println("Enter your salary :");
+        salary = scanner.nextDouble();
+        scanner.close();
 
-        System.out.println("Enter Initials:  ");
-        init=sc.next().charAt(0);
+        salary = salary + (salary * .10);
 
-        System.out.println("Enter Your Firstname:  ");
-        firstname=sc.nextLine();
-
-        System.out.println("Enter Your Fullname:  ");
-        fullname=sc.nextLine();
-
-        System.out.println("Enter Your Marks:  ");
-        marks=sc.nextFloat();
-
-        System.out.println("Enter Your Salary:  ");
-        salary=sc.nextDouble();
-
-        sc.close();
-
-
-
-        salary=salary+(salary*0.10);
-
-
-        System.out.println("************ Wipro Employee Details *************8");
-
-        System.out.println("Employee id  "+ id);
-
-
-        System.out.println("Enter Initials:  "+init);
-
-
-        System.out.println("Enter Your Firstname  :"+firstname);
-
-
-        System.out.println("Enter Your Fullname:  "+fullname);
-
-
-        System.out.println("Enter Your Marks:  "+marks);
-
-
-        System.out.println("Enter Your Salary:  "+salary);
-
-
+        System.out.println("******************** Wipro Employee Details **************************");
+        System.out.println("Employee Id : "+id);
+        System.out.println("Full name   : "+init+" "+fullName);
+        System.out.println("First name  : "+firstName);
+        System.out.println("Marks       : "+marks);
+        System.out.println("Salary      : "+salary);
+        System.out.println("************************************************************************");
     }
 }
